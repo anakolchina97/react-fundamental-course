a81cd8c50a2947ddf92146d092fca34b37c87529
Annie <anniehartmann97@gmail.com>
2023-01-07 21:43:12 +0300

    Fix

--- src/hooks/useFetching.js
+++ src/hooks/useFetching.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export const useFetching = (callback) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const fetching = async (...args) => {
     try {
